8fc884281858c72188c2d794846ab563ad92bed9
Joshua Allford <joshallfordnanook@gmail.com>
2021-10-13 17:35:42 +0100

    dog's dinner

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+    def name 
+        @name
+    end
+    def job=(job)
+        @job = job
+    end
+    def job
+        @job
+    end
 
 end
+
+josh = Person.new
+josh.name = "Joshua"
+josh.job = "Software Engineer"
+
+puts josh.job
+puts josh.name
